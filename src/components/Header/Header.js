9e26b06f5e267f17a7f80571aee8c8c5680816d7
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2022-06-19 13:56:59 +0530

    ft. header structure + design

---
+++ src/components/Header/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  MenuAlt2Icon,
+  CakeIcon,
+  HeartIcon,
+  ShoppingBagIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+import HeaderIcon from "./HeaderIcon";
+
+function Header() {
+  return (
+    <div className="flex items-center px-10 py-6 uppercase w-full justify-between gap-x-80 shadow-md">
+      <div className="w-1/2 flex justify-around items-center">
+        <HeaderIcon Icon={MenuAlt2Icon} active />
+        <HeaderIcon Icon={CakeIcon} />
+        {/* <div className="pl-10 py-0 border-l-2 border-l-sl">new</div> */}
+        <div>new</div>
+        <div>sale</div>
+        <div>men</div>
+        <div>women</div>
+      </div>
+
+      <div className="w-1/2 flex justify-around items-center gap-x-40">
+        <div className="w-1/2 flex justify-around items-center">
+          <HeaderIcon Icon={HeartIcon} />
+          <div>search</div>
+        </div>
+        <div className="w-1/2 flex justify-around items-center">
+          <HeaderIcon Icon={ShoppingBagIcon} />
+          <HeaderIcon Icon={UserIcon} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/HeaderIcon.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HeaderIcon({ Icon,active }) {
+  return (
+    <div className="flex items-center cursor-pointer active:border-b-5 active:border-blue-600 md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-xl group">
+      <Icon className= {`h-5 text-gray-500 text-center sm:h-7 mx-auto group-hover:text-gray-900 ${active && "text-sd"}` }/>
+    </div>
+  );
+}
+
+export default HeaderIcon;
