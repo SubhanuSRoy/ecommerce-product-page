8031a29eb007acce393c3c63b518f81c8af97773
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2022-06-20 00:03:26 +0530

    ft. main design updates

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,41 +1,57 @@
 import React from "react";
+import Image from "next/image";
 import styles from "../../styles/Main.module.css";
 import pullover from "../../assets/pulloverImg.png";
 import skirt from "../../assets/skirtImg.png";
 import HeaderIcon from "../Header/HeaderIcon";
-import {PlusIcon} from "@heroicons/react/solid";
+import { PlusIcon } from "@heroicons/react/solid";
 
 function Main() {
   return (
     <div className={styles.main}>
       <div className="flex items-end justify-between px-4">
         {/* name + add button */}
-        <div className="flex items-center justify-between pb-10 px-20 w-120">
-            <div className="text-4xl uppercase font-bold">Tied Green <br /> V Neck Shirt</div>
-            <div><PlusIcon className=" h-5 text-gray-500 text-center sm:h-7  rounded-full bg-black p-2 shadow-md text-white"/></div>
+        <div className="flex items-center justify-between pb-20 px-20 w-5/12">
+          <div className="text-4xl uppercase font-bold">
+            Tied Green <br /> V Neck Shirt
+          </div>
+          <div>
+            <PlusIcon className=" h-5 text-gray-500 text-center sm:h-7  rounded-full bg-black p-2 shadow-md text-white" />
+          </div>
         </div>
 
         {/* outfit breakdown cards */}
-        <div className="flex flex-col w-1/3 px-6 py-20 ">
+        <div className="flex flex-col w-1/3 px-6 py-20 gap-y-6">
           {/* top */}
-          <div className="rounded-md p-4 flex items-center justify-between shadow-md">
+          <div
+            className="rounded-2xl p-4 flex items-center justify-between shadow-md h-40 w-80"
+            style={{ position: "relative" }}
+          >
             {/* name and price */}
-            <div className="flex flex-col items-start justify-between">
-              <div>Half Dome Pullover</div>
+            <div className="flex flex-col items-start justify-around h-full">
+              <div className="w-1/3 flex-wrap">Half Dome Pullover</div>
               <div className="text-tp font-extrabold">$87</div>
             </div>
             {/* image */}
-            <img src={pullover} width="50%" alt="pullover" />
+            <Image
+              src={pullover}
+              layout="fill"
+              objectFit="contain"
+              alt="pullover"
+            />
           </div>
           {/* bottom */}
-          <div className="rounded-md p-4 flex items-center justify-between shadow-md h-40">
+          <div
+            className="rounded-2xl p-4 flex items-center justify-between shadow-md h-40 w-80"
+            style={{ position: "relative" }}
+          >
             {/* name and price */}
             <div className="flex flex-col items-start justify-around h-full">
               <div>Skirt</div>
               <div className="text-tp font-extrabold">$20</div>
             </div>
             {/* image */}
-            <img src={skirt} className="w-1/2" alt="skirt" />
+            <Image src={skirt} layout="fill" objectFit="contain" alt="skirt" />
           </div>
         </div>
       </div>
